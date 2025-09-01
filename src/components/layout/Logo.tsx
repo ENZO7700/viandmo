97fdf930c4f0074493b9f1c5ca6c350f094b4d41
ENZO F <enzoenzof2024@gmail.com>
2025-09-01 23:22:28 +0000

    File changes

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -6,12 +6,11 @@ export default function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("relative flex items-center h-12 w-40", className)}>
       <Image 
-        src="https://picsum.photos/seed/logo/200/60" 
-        alt="Papi Hair Design PRO Logo" 
+        src="/viandmo_logo_regular_land.svg" 
+        alt="VI&MO Logo" 
         fill
         priority
         className="object-contain"
-        data-ai-hint="logo design"
       />
     </Link>
   );
