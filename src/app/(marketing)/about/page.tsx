622a8dde882901c8e9839d392a83e2d318eb71ec
ENZO F <enzoenzof2024@gmail.com>
2025-10-16 00:50:13 +0000

    suhlasim !

--- src/app/(marketing)/about/page.tsx
+++ src/app/(marketing)/about/page.tsx
@@ -218,5 +218,3 @@ export const metadata: Metadata = {
     url: '/about',
   },
 };
-
-    
--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -39,5 +39,3 @@ export default function MarketingLayout({ children }: MarketingLayoutProps) {
     </div>
   );
 }
-
-    
